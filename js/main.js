0dd43b2141af4821433f2061d9844161b19fb542
SinyakovaZhenya <116831173+SinyakovaZhenya@users.noreply.github.com>
2023-05-26 21:17:39 +0300

    26.05
    26.05

--- js/main.js
+++ js/main.js
@@ -2,6 +2,23 @@ const mobileNavBtn = document.querySelector('.mobile-nav-button');
 const mobileNavIcon = document.querySelector('.mobile-nav-button__icon');
 const mobileNav = document.querySelector('.mobile-nav');
 const filterСard = document.querySelectorAll('.card');
+const topBtn = document.querySelector('.topBtn');
+
+window.addEventListener('scroll', function() {
+	if(window.pageYOffset > 400) {
+		topBtn.classList.add('top-show');
+	}
+	else{
+		topBtn.classList.remove('top-show');
+	}
+})
+
+topBtn.addEventListener('click', function topHandler() {
+	if(window.pageYOffset > 0) {
+		window.scrollBy(0, -50);
+		setTimeout(topHandler, 0);
+	}
+})
 
 mobileNavBtn.addEventListener('click', function() {
 	mobileNavIcon.classList.toggle('active');
